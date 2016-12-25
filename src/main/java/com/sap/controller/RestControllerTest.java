0d3d92ae7f5c961ea7469a3bcf2cca0fffde5c6c
unknown <cle.ee@163.com>
2016-12-25 12:19:25 +0800

    rest controller

--- src/main/java/com/sap/controller/RestControllerTest.java
+++ src/main/java/com/sap/controller/RestControllerTest.java
@@ -1,11 +1,8 @@
 package com.sap.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 //@CrossOrigin
